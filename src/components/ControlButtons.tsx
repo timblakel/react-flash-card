acf3f80d4062657c08482d00052e84d199cb1865
timblakel <timblake@udel.edu>
2021-09-23 19:20:57 -0400

    Laying out ControlButtons

--- src/components/ControlButtons.tsx
+++ src/components/ControlButtons.tsx
@@ -1,8 +1,15 @@
-import { Button, Col } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 
 export function ControlButtons(): JSX.Element {
     return <Col>
-        <Button variant="primary">Primary</Button>
+        <Container>
+            <Row>
+                <Button variant="primary" className="m-2">Flip Card</Button>
+            </Row>
+            <Row>
+                <Button variant="secondary" className="m-2">Next Card</Button>
+            </Row>
+        </Container>
     </Col>
 }
