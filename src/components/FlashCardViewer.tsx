d4a863f4e92b4ec89ff2458ee889324ded5b00f9
timblakel <timblake@udel.edu>
2021-09-23 18:46:40 -0400

    Create FlashCardViewer component

---
+++ src/components/FlashCardViewer.tsx
@@ -0,0 +1,14 @@
+import { Col, Card } from "react-bootstrap";
+
+
+export function FlashCardViewer(): JSX.Element {
+    return <Col>
+        <Card>
+            <Card.Body>
+                <Card.Text>
+                    Sample Card Text
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
+}
