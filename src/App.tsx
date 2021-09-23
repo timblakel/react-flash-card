03f182acabde8204ecbbb2f63ba1765253979827
timblakel <timblake@udel.edu>
2021-09-23 19:04:20 -0400

    Basic column/row layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { FlashCardViewer } from './components/FlashCardViewer';
 
 function App() {
   return (
-    <div className="App">
-      <FlashCardViewer></FlashCardViewer>
-    </div>
+    <Container className="App">
+      <Row>
+        <Col>
+          Placeholder
+          This will be control buttons
+        </Col>
+        <FlashCardViewer></FlashCardViewer>
+      </Row>
+    </Container>
   );
 }
 
