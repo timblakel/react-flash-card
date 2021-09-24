e6421fa7d3e3e6777cfbc97f839b9fe5537113d3
timblakel <timblake@udel.edu>
2021-09-23 20:37:21 -0400

    edit flash card buttons

--- src/components/EditFlashCards.tsx
+++ src/components/EditFlashCards.tsx
@@ -1,15 +1,21 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 
 export function EditFlashCards(): JSX.Element {
 
-    return <Container>
+    return <Container className="border border-info p-2 m-4">
         <Row>
-            <Col>Add cards placeholder</Col>
-                <Col>Edit above card placeholder</Col>
-            </Row>
-            <Row>
-                <Col>Save placeholder</Col>
-            </Row>
-        </Container>
+            <Col>
+                <Button variant="secondary">Add Flash Card</Button>
+            </Col>
+            <Col>
+                <Button variant="secondary">Edit Current Card placeholder</Button>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Button variant="secondary">Save placeholder</Button>
+            </Col>
+        </Row>
+    </Container>
 }
