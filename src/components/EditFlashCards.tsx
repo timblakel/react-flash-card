95af4827c5dc6ebf334b277f944e313a6306636c
timblakel <timblake@udel.edu>
2021-09-23 21:40:41 -0400

    adjust add flash card layout

--- src/components/EditFlashCards.tsx
+++ src/components/EditFlashCards.tsx
@@ -3,16 +3,31 @@ import { flashCard } from "../interfaces/flashCard";
 
 const LOCAL_STORAGE_CARDS = 'flash-cards';
 
-export function EditFlashCards({cardPile}: {cardPile: flashCard[]}): JSX.Element {
+export function EditFlashCards({cardPile, setCardPile}: {cardPile: flashCard[], setCardPile: (f: flashCard[])=>void}): JSX.Element {
 
     function saveCards(): void {
         localStorage.setItem(LOCAL_STORAGE_CARDS,JSON.stringify(cardPile));
     }
 
+    function addCard(): void {
+        console.log('Add card');
+    }
+
     return <Container className="border border-info p-2 m-4">
         <Row>
             <Col>
-                <Button variant="secondary">Add Flash Card</Button>
+                <Container>
+                    <Row>
+                        <Col>
+                            Add flash card form
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button variant="secondary">Add Flash Card</Button>
+                        </Col>
+                    </Row>
+                </Container>
             </Col>
             <Col>
                 <Button variant="secondary">Edit Current Card placeholder</Button>
