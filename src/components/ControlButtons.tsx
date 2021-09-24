2cc15cce63753089f2dc8b32bb6ed0f86027f9a1
timblakel <timblake@udel.edu>
2021-09-23 21:22:09 -0400

    flipCard sets cardPile

--- src/components/ControlButtons.tsx
+++ src/components/ControlButtons.tsx
@@ -1,11 +1,24 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { flashCard } from "../interfaces/flashCard";
 
-export function ControlButtons({setCurrentCard, currentCard}: {setCurrentCard: (f: flashCard) => void, currentCard: flashCard}): JSX.Element {
+export function ControlButtons({setCurrentCard, currentCard, setCardPile, cardPile}: {
+        setCurrentCard: (f: flashCard) => void, 
+        currentCard: flashCard,
+        setCardPile: (f: flashCard[]) => void,
+        cardPile: flashCard[]
+    }): JSX.Element {
+    
     function flipCard() {
         let flippedCard: flashCard = {...currentCard};
         flippedCard.isFront = !flippedCard.isFront;
         setCurrentCard(flippedCard);
+        // console.log(`flipped card: ${JSON.stringify(flippedCard)}`);
+        // console.log(`cardPile ${JSON.stringify(cardPile)}`);
+        // console.log(`currentCard: ${JSON.stringify(currentCard)}`);
+        let currInd: number = cardPile.indexOf(currentCard,0);
+        let newPile: flashCard[] = [...cardPile];
+        newPile[currInd] = flippedCard;
+        setCardPile(newPile);
     }
 
     return <Col>
