f400084584ba024760dc5ab71a28d7f946be6c0c
timblakel <timblake@udel.edu>
2021-09-23 22:24:31 -0400

    addCard functionality

--- src/components/EditFlashCards.tsx
+++ src/components/EditFlashCards.tsx
@@ -1,16 +1,27 @@
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { flashCard } from "../interfaces/flashCard";
+import { FlashCardViewer } from "./FlashCardViewer";
 
 const LOCAL_STORAGE_CARDS = 'flash-cards';
 
 export function EditFlashCards({cardPile, setCardPile}: {cardPile: flashCard[], setCardPile: (f: flashCard[])=>void}): JSX.Element {
+    const [newFront, setNewFront] = useState<string>("Front");
+    const [newBack, setNewBack] = useState<string>("Back");
 
     function saveCards(): void {
         localStorage.setItem(LOCAL_STORAGE_CARDS,JSON.stringify(cardPile));
     }
 
     function addCard(): void {
-        console.log('Add card');
+        let newId: number = cardPile[cardPile.length-1].id + 1;
+        let newCard: flashCard = {
+            id: newId,
+            front: newFront,
+            back: newBack,
+            isFront: true
+        }
+        setCardPile([...cardPile, newCard]);
     }
 
     return <Container className="border border-info p-2 m-4 ml-auto">
@@ -19,14 +30,20 @@ export function EditFlashCards({cardPile, setCardPile}: {cardPile: flashCard[], 
                 <Form>
                     <Form.Group className="mb-1" controlId="newCardFront">
                         <Form.Label>Front</Form.Label>
-                        <Form.Control type="text" placeholder="card front" />
+                        <Form.Control type="text" placeholder="card front"
+                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                            setNewFront(event.target.value);
+                        }} />
                     </Form.Group>
 
                     <Form.Group className="mb-2" controlId="newCardBack">
                         <Form.Label>Back</Form.Label>
-                        <Form.Control type="text" placeholder="card back" />
+                        <Form.Control type="text" placeholder="card back" 
+                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                            setNewBack(event.target.value);
+                        }} />
                     </Form.Group>
-                    <Button variant="secondary">Add Flash Card</Button>
+                    <Button onClick={addCard} variant="secondary">Add Flash Card</Button>
                 </Form>
             </Col>
             <Col>
