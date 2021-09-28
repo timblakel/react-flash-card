4cc9c128c5a9580d44cc505b545627e57f685a35
timblakel <timblake@udel.edu>
2021-09-27 21:43:44 -0400

    removed Col and BUtton from react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { FlashCardViewer } from './components/FlashCardViewer';
