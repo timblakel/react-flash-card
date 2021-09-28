0b229896eb6ba5e91341f1c095201b8b8365f3ef
timblakel <timblake@udel.edu>
2021-09-28 11:58:06 -0400

    Flip Card test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import {fireEvent, within} from '@testing-library/dom'
 import App from './App';
 
 test('title is visible', () => {
@@ -13,3 +14,20 @@ test('card is visible', ()=> {
   const cardElement = screen.getByTestId("flashcard");
   expect(cardElement).toBeInTheDocument();
 });
+
+test('front of card is showing on startup', () => {
+  render(<App />);
+  const cardElement = screen.getByTestId("flashcard");
+  expect(within(cardElement).getByText('Flashcard Front'));
+});
+
+test('back of card shows on Flip Card button click', async () => {
+  render(<App />);
+
+   // Maybe replace this with testID
+  const flipCardButton = screen.getByText("Flip Card");
+  fireEvent.click(flipCardButton);
+
+  const cardBack = await within(screen.getByTestId("flashcard")).findByText("Flashcard Back");
+  expect(cardBack).toBeInTheDocument();
+})
