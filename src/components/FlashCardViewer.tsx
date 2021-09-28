e4204bacde707da0cad0a94845407bd013558a6c
timblakel <timblake@udel.edu>
2021-09-27 23:09:02 -0400

    Front/Back card header

--- src/components/FlashCardViewer.tsx
+++ src/components/FlashCardViewer.tsx
@@ -1,9 +1,21 @@
 import { Col, Card } from "react-bootstrap";
 import { flashCard } from "../interfaces/flashCard";
 
+
+
 export function FlashCardViewer({currentCard}: {currentCard: flashCard}): JSX.Element {
+
+    function getCardHeader(): string {
+        if (currentCard.isFront) {
+            return "Flashcard Front";
+        } else {
+            return "Flashcard Back";
+        }
+    }
+
     return <Col>
         <Card>
+            <Card.Header>{getCardHeader()}</Card.Header>
             <Card.Body>
                 {currentCard.isFront && <Card.Text>
                     {currentCard.front}
